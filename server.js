e8230ebc513c2f53ad827af6c8f87eb1eb0f5e18
riyadameri <riyadammmeri@gmail.com>
2026-01-23 16:57:14 +0100

    mvp

--- server.js
+++ server.js
@@ -7,7 +7,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://bonprix-bqvx.onrender.com',
+        'http://localhost:3000',
+        'http://127.0.0.1:5500', // For local testing
+        'http://127.0.0.1:5501', // For local testing
+        'http://localhost:5000',  // For local testing
+        'http://localhost:3000',  // For local testing
+        
+
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Handle preflight requests
+app.options('*', cors());
+
 app.use(express.json({ limit: '50mb' })); // زيادة الحد للصور الكبيرة
 app.use(express.static('frontend'));
 
